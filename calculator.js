46cb7f4ade22a001430059d54c04973ac3b05e11
Kieran <kserra@umass.edu>
2024-10-11 12:51:22 -0400

    complete natural Log function

--- calculator.js
+++ calculator.js
@@ -23,6 +23,10 @@ function calculateReciprocal() {
 
 // TODO: Implement natural log function
 function naturalLog(value) {
+    if (value <= 0) {
+        return "Invalid input";
+    }
+    return Math.log(value);
     // return the natural log of the value
 }
 
